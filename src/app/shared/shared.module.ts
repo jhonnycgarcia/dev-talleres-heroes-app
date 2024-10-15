1680a53a242de0e4c1d36e40bb341c52e4e2d25d
Jhonny <jhonnycgarcia@gmail.com>
2024-10-14 21:29:06 -0500

    Creación de componentes - Pages

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    Error404PageComponent,
+  ],
+  exports: [
+    Error404PageComponent,
+  ]
+})
+export class SharedModule { }
