b4706ae2ef055ba3480e67c8036f34f132ca7c8c
Jhonny <jhonnycgarcia@gmail.com>
2024-10-15 00:04:40 -0500

    Obtener Héroe por URL

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesSrv: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesSrv.getHeroById(id)),
+      )
+      .subscribe((hero) => {
+        if(!hero){ return this.router.navigate(['/heroes/list']); }
+        return this.hero = hero;
+      });
+  }
 
 }
